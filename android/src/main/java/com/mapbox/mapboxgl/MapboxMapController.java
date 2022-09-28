8e30ea261858e1d520d75eaa4e10696e7abc0055
Philipp Matthes <github@philippmatth.es>
2022-09-28 10:57:24 +0200

    Add wrapper for location engine to support custom user locations (#1)
    * Add wrapper for location engine to support custom user locations
    
    * Add null check for custom location; Add documentation; Small refactoring
    
    * Add Override annotations
    
    Co-authored-by: adeveloper-wq <dajesch@gmail.com>

--- android/src/main/java/com/mapbox/mapboxgl/MapboxMapController.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxMapController.java
@@ -118,7 +118,7 @@ final class MapboxMapController
   private boolean dragEnabled = true;
   private MethodChannel.Result mapReadyResult;
   private LocationComponent locationComponent = null;
-  private LocationEngine locationEngine = null;
+  private CustomLocationManager locationEngine = null;
   private LocationEngineCallback<LocationEngineResult> locationEngineCallback = null;
   private LocalizationPlugin localizationPlugin;
   private Style style;
@@ -267,7 +267,7 @@ final class MapboxMapController
   @SuppressWarnings({"MissingPermission"})
   private void enableLocationComponent(@NonNull Style style) {
     if (hasLocationPermission()) {
-      locationEngine = LocationEngineProvider.getBestLocationEngine(context);
+      locationEngine = new CustomLocationManager(LocationEngineProvider.getBestLocationEngine(context));
       locationComponent = mapboxMap.getLocationComponent();
       locationComponent.activateLocationComponent(
           context, style, buildLocationComponentOptions(style));
@@ -628,7 +628,23 @@ final class MapboxMapController
         }
       case "map#updateUserLocation":
         {
-          // TODO: Add support for updating the user location.
+          if (this.myLocationEnabled && locationComponent != null && locationEngine != null) {
+            // Check if a proper location is given, if not put null as argument to the "overrideLastLocation"
+            // function such that the fallback location engine is being used.
+            if (call.argument("lat") == null || call.argument("lon") == null){
+              locationEngine.overrideLastLocation(null);
+            } else {
+              Location newLocation = new Location("");
+              newLocation.setLatitude(call.argument("lat"));
+              newLocation.setLongitude(call.argument("lon")); 
+              if (call.argument("alt") != null) newLocation.setAltitude(call.argument("alt"));
+              if (call.argument("acc") != null) newLocation.setAccuracy((float)(double)call.argument("acc"));
+              if (call.argument("heading") != null) newLocation.setBearing((float)(double)call.argument("heading"));
+              if (call.argument("speed") != null)  newLocation.setSpeed((float)(double)call.argument("speed"));
+              newLocation.setTime(System.currentTimeMillis()); 
+              locationEngine.overrideLastLocation(newLocation);
+            }
+          }
           result.success(null);
           break;
         }
